78ecd5ca494ae5ea5602b49bc04911ba7cfeb380
Nick <nickdloucks@outlook.com>
2021-12-16 20:56:44 -0800

    start writing DB connection async function

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,6 +2,9 @@ import * as mongoDB from 'mongodb';
 import * as dotenv from 'dotenv';
 // import model(s) here
 
-export async function connectDB(params:type) {
-    
+export async function connectDB() {
+    dotenv.config();
+    const client = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+    await client.connect();
+    const db = client.db(process.env.DB_NAME);
 }
