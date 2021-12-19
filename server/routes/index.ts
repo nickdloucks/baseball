7f4b28979b071a5bad82a3c2f2146b1046d7d4c6
Nick <80440799+nickdloucks@users.noreply.github.com>
2021-12-18 19:20:49 -0700

    Update index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 // import model(s) here
 
 export async function connectDB() {
-    dotenv.config();
+    dotenv.config(); // pull in the environment variables to connect to database
     const client = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
     await client.connect();
     const db = client.db(process.env.DB_NAME);
